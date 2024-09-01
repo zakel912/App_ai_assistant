f1e32e38ac7d939d036215891c07cd9ba9a3d579
zakel912 <153775450+zakel912@users.noreply.github.com>
2024-09-01 20:48:58 +0200

    Update response.py

--- response.py
+++ response.py
@@ -1,7 +1,6 @@
-
+# Generate a response based on simple keyword matching rules.
 def get_response(msg):
     
-    ### Rule based response ###
     if "create" in msg:
         response = "Compte crée"
     elif "delete" in msg:
@@ -9,24 +8,3 @@ def get_response(msg):
     else:
         response = "Je n'ai pas compris"
     return (response)
-
-
-
-def main():
-    print("Bot: Hello! How can I assist you today?")
-    
-    while True:
-        user_message = input("You: ").strip().lower()
-        
-        ### Rule based response ###
-        if "create" in user_message:
-            response = "Compte crée"
-        elif "delete" in user_message:
-            response = "Compte supprimé"
-        else:
-            response = "Je n'ai pas compris"
-        print(f"Bot: {response}")
-
-
-if __name__ == "__main__":
-    main()
